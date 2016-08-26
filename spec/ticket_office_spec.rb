c0b3da3cbb7c966c5c4343b547e9e2d295b349ca
Hemal <h.n.varambhia@gmail.com>
2016-08-26 12:12:25 +0100

    The ticket office returns a JSON document of the reservation that was made

--- spec/ticket_office_spec.rb
+++ spec/ticket_office_spec.rb
@@ -1,3 +1,4 @@
+require 'json'
 describe 'Ticket office' do
   class TicketOffice
     def initialize train_data_service
@@ -5,20 +6,36 @@ describe 'Ticket office' do
     end
 
     def make_reservation request
-      @train_data_service.reserve_seats request
+      @train_data_service.reserve_seats(request).to_json
     end
   end
   
   describe '#make_reservation' do
-    it 'makes a reservation for the given request' do
-      request = { train_id: 'train_1234', seats: 1 }
-      train_data_service = double :train_data_service
-      expect(train_data_service).to receive(:reserve_seats).with request
-
-      TicketOffice.new(train_data_service).make_reservation request
+    before :each do
+      @train_data_service = double :train_data_service
+      @ticket_office = TicketOffice.new @train_data_service
     end
 
-    it 'returns a JSON document detailing the reservation made'
+    it 'makes a reservation for the given request' do
+      request = { train_id: 'train_1234', seats: 1 }
+      expect(@train_data_service).to receive(:reserve_seats).with request
+
+      @ticket_office.make_reservation request
+    end
+
+    it 'returns a JSON document detailing the reservation made' do
+      request = { train_id: 'train_1234', seats: 1 }
+      reservation = {
+        train_id: 'train_1234', booking_reference: 'abc', seats: %w{1A}
+      }
+      allow(@train_data_service).to(
+        receive(:reserve_seats).with(request).and_return reservation)
+
+      reservation_doc = @ticket_office.make_reservation request
+
+      expect(reservation_doc).to eq reservation.to_json
+    end
+
     it 'reserves any number of seats on any train'
   end
 end
