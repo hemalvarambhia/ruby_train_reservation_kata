ad18ecb05c9732c0ed7b6d7fbde35e0a916d8cf1
Hemal N Varambhia <h.n.varambhia@gmail.com>
2016-08-27 14:54:08 +0100

    When the train has no existing reservations, a seat is reserved

--- spec/train_data_service_spec.rb
+++ spec/train_data_service_spec.rb
@@ -3,20 +3,32 @@ describe 'Train Data Service' do
   class TrainDataService
     def initialize(train_data_api, booking_reference_service)
       @train_data_api = train_data_api
+      @booking_reference = booking_reference_service
     end
 
     def reserve_seats request
       seats_on_train = 
         JSON.parse(@train_data_api.seats_for(request[:train_id]))['seats']
       if seats_on_train.all? { |_id, seat| booked? seat }
-        no_reservation_on request[:train_id]
-      end      
+       return  no_reservation_on request[:train_id]
+      end
+
+      free_seats = seats_on_train.select { |_id, seat| free? seat }
+      {
+        train_id: request[:train_id],
+        booking_reference: @booking_reference.new_reference_number,
+        seats: free_seats.keys.first(1)
+      }
     end
 
-    private 
+    private
 
     def no_reservation_on train
       { train_id: train, booking_reference: '', seats: [] }
+    end
+
+    def free? seat
+      seat['booking_reference'].empty?
     end
 
     def booked? seat
@@ -83,7 +95,48 @@ describe 'Train Data Service' do
       end
 
       context 'when the train has no existing reservations' do
-        it 'reserves the first available seat'
+        before :each do
+          booking_reference =
+            double(:booking_reference,
+                   new_reference_number: 'a_reference_number')
+          @train_data_service = TrainDataService.new(
+            @train_data_api, booking_reference
+          )
+        end
+        it 'reserves the first available seat' do
+          allow(@train_data_api).to(
+            receive(:seats_for).with('train_1234').and_return(
+              seats_doc(free(1, 'A'), free(2, 'A'), free(3, 'A')) 
+            )
+          )
+
+          reservation = @train_data_service.reserve_seats @request
+
+          expect(reservation).to(
+            eq(
+              train_id: 'train_1234',
+              booking_reference: 'a_reference_number',
+              seats: %w{1A}
+            )
+          )
+        end
+
+        
+        def seats_doc *seats
+          { 'seats' => Hash[seats] }.to_json
+        end
+
+        def free(seat_number, coach)
+          [
+            "#{seat_number}#{coach}",
+            {
+              'booking_reference' => '',
+              'seat_number' => seat_number,
+              'coach' => coach
+            }
+          ]
+        end
+
       end
       
       context 'when the train is under 70% reserved' do
