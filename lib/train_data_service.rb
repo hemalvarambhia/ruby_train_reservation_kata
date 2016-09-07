6e0f076c18499225d1243d3730fe82f46de1a4d4
Hemal <h.n.varambhia@gmail.com>
2016-09-07 13:34:42 +0100

    Extracted a Train class as TrainDataService had more than one responsibility.

--- lib/train_data_service.rb
+++ lib/train_data_service.rb
@@ -12,10 +12,9 @@ class TrainDataService
   end
 
   def reserve_seats request
-    @seats_on_train = seats_on request[:train_id]
-    
+    @train = train_with_id request[:train_id]
     reservation = 
-      if percentage_including(request) > 70.percent
+      if @train.percentage_including(request) > 70.percent
         no_reservation_on request[:train_id]
       else
         make_reservation request
@@ -32,39 +31,46 @@ class TrainDataService
 
   private
 
+  def train_with_id train_id
+    seats_doc = JSON.parse(
+      @train_data_api.seats_for(train_id), symbolize_names: true)[:seats]
+
+    Train.new(seats_doc
+      .map { |_id, seat| Seat.new seat }
+      .sort_by { |seat| seat.seat_number }
+    )
+  end
+
   def make_reservation request
     {
       train_id: request[:train_id],
       booking_reference: @booking_reference.new_reference_number,
-      seats: free_seats(request[:seats])
+      seats: @train.free_seats(request[:seats])
     }
-  end
-
-  def seats_on train_id 
-    seats_doc = JSON.parse(
-      @train_data_api.seats_for(train_id), symbolize_names: true)[:seats]
-
-    seats_doc
-      .map { |_id, seat| Seat.new seat }
-      .sort_by { |seat| seat.seat_number }    
-  end
-
-  def percentage_including request
-    number_booked = 
-      @seats_on_train.count { |seat| seat.booked? } + request[:seats]
-
-    Rational(number_booked, @seats_on_train.size)
-  end
-
-  def free_seats number
-    free_seats = 
-      @seats_on_train.select { |seat| seat.free? }.map { |seat| seat.id}
-
-    free_seats.first(number)
   end
 
   def no_reservation_on train
     { train_id: train, booking_reference: '', seats: [] }
+  end
+
+  class Train
+    def initialize seats
+      @seats_on_train = seats
+    end
+
+    def percentage_including request
+      number_booked = 
+        @seats_on_train.count { |seat| seat.booked? } + request[:seats]
+
+      Rational(number_booked, @seats_on_train.size)
+    end
+
+    def free_seats number
+      free_seats = 
+        @seats_on_train.select { |seat| seat.free? }.map { |seat| seat.id}
+
+      free_seats.first(number)
+    end
   end
 
   class Seat
