110e531279dacb5108c5f21cb9197264e2bbe8c8
Hemal <h.n.varambhia@gmail.com>
2016-09-12 12:39:40 +0100

    Added the reserve method

--- lib/train_data_api.rb
+++ lib/train_data_api.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'json'
 class TrainDataAPI
   def initialize uri
     @uri = uri
@@ -6,5 +7,17 @@ class TrainDataAPI
 
   def seats_for train_id
     Net::HTTP.get(URI("#{@uri}/data_for_train/#{train_id}"))
+  end
+
+  def reserve reservation
+    with_seats_in_json = {
+      train_id: reservation[:train_id],
+      booking_reference: reservation[:booking_reference],
+      seats: reservation[:seats].to_json
+    }
+    response = 
+      Net::HTTP.post_form(URI("#{@uri}/reserve"), with_seats_in_json)
+
+    response.body
   end
 end
